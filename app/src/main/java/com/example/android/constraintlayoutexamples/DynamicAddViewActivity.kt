0c7b70ebf7b74dfb1b5893cd1270ce7d3fd88a74
dumingwei <dumingwei07050@hellobike.com>
2019-06-09 17:42:14 +0800

    Add guideline

--- app/src/main/java/com/example/android/constraintlayoutexamples/DynamicAddViewActivity.kt
+++ app/src/main/java/com/example/android/constraintlayoutexamples/DynamicAddViewActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.constraint.ConstraintLayout
 import android.support.constraint.ConstraintSet
+import android.support.constraint.Guideline
 import android.support.v7.app.AppCompatActivity
 import android.transition.TransitionManager
 import android.view.ViewGroup
@@ -26,15 +27,19 @@ class DynamicAddViewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dynamic_add_view)
+        //setContentView(R.layout.activity_dynamic_add_view)
         //addView()
         //addViewPartView()
         //changeLayoutParams()
         //changeConstraintSet()
 
-        ivLeft.setOnClickListener {
-            changeConstraintSet()
-        }
+        /* ivLeft.setOnClickListener {
+             changeConstraintSet()
+         }*/
+
+        addGuideLine()
+
+        //addViewUseConstraintSet()
     }
 
     /**
@@ -106,6 +111,114 @@ class DynamicAddViewActivity : AppCompatActivity() {
 
     }
 
+
+    /**
+     * Add View use ConstraintSet
+     */
+    private fun addViewUseConstraintSet() {
+        val constraintLayout = ConstraintLayout(this)
+        constraintLayout.id = R.id.cl_root
+        constraintLayout.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        //先设置根布局
+        setContentView(constraintLayout)
+
+        val constraintSet = ConstraintSet()
+
+        val ivLeft = ImageView(this)
+        ivLeft.id = R.id.ivLeft
+        ivLeft.scaleType = ImageView.ScaleType.CENTER_CROP
+        ivLeft.setImageResource(R.drawable.lake)
+
+        constraintSet.connect(R.id.ivLeft, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START,
+                ScreenUtil.dpToPx(this, 8))
+        constraintSet.connect(R.id.ivLeft, ConstraintSet.END, R.id.tvRight, ConstraintSet.START)
+        constraintSet.connect(R.id.ivLeft, ConstraintSet.TOP, R.id.tvRight, ConstraintSet.TOP)
+        constraintSet.connect(R.id.ivLeft, ConstraintSet.BOTTOM, R.id.tvRight, ConstraintSet.BOTTOM)
+
+        constraintSet.constrainHeight(R.id.ivLeft, 0)
+        constraintSet.constrainWidth(R.id.ivLeft, 0)
+        constraintSet.setDimensionRatio(R.id.ivLeft, "h,16:9")
+
+        val tvRight = TextView(this)
+        tvRight.id = R.id.tvRight
+        tvRight.text = getString(R.string.lake_tahoe_title)
+        tvRight.textSize = 30F
+
+        constraintSet.constrainHeight(R.id.tvRight, ConstraintLayout.LayoutParams.WRAP_CONTENT)
+        constraintSet.constrainWidth(R.id.tvRight, ConstraintLayout.LayoutParams.WRAP_CONTENT)
+        constraintSet.connect(R.id.tvRight, ConstraintSet.START, R.id.ivLeft, ConstraintSet.END,
+                ScreenUtil.dpToPx(this, 8))
+        constraintSet.connect(R.id.tvRight, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP,
+                ScreenUtil.dpToPx(this, 16))
+
+
+        val tvBottom = TextView(this)
+        tvBottom.id = R.id.tvBottom
+        tvBottom.text = getString(R.string.lake_discription)
+
+        constraintSet.connect(R.id.tvBottom, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START,
+                ScreenUtil.dpToPx(this, 8))
+
+        constraintSet.connect(R.id.tvBottom, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END,
+                ScreenUtil.dpToPx(this, 8))
+
+        constraintSet.connect(R.id.tvBottom, ConstraintSet.TOP, R.id.tvRight, ConstraintSet.BOTTOM,
+                ScreenUtil.dpToPx(this, 24))
+
+        //设置高度
+        constraintSet.constrainHeight(R.id.tvBottom,ConstraintLayout.LayoutParams.WRAP_CONTENT)
+
+        constraintLayout.addView(ivLeft)
+        constraintLayout.addView(tvRight)
+        constraintLayout.addView(tvBottom)
+
+        TransitionManager.beginDelayedTransition(constraintLayout)
+
+        constraintSet.applyTo(constraintLayout)
+    }
+
+
+    private fun addGuideLine() {
+        val constraintLayout = ConstraintLayout(this)
+        constraintLayout.id = R.id.cl_root
+        constraintLayout.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT)
+        //先设置根布局
+        setContentView(constraintLayout)
+
+        val guideline = Guideline(this)
+        guideline.id = R.id.guideline
+
+        val guideLayoutParams: ConstraintLayout.LayoutParams = ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT)
+        guideLayoutParams.guidePercent = 0.4f
+        guideLayoutParams.topToTop = R.id.cl_root
+        guideLayoutParams.bottomToBottom = R.id.cl_root
+        //注意
+        guideLayoutParams.orientation = ConstraintLayout.LayoutParams.HORIZONTAL
+
+        guideline.layoutParams = guideLayoutParams
+
+        constraintLayout.addView(guideline)
+
+        val ivLeft = ImageView(this)
+        ivLeft.id = R.id.ivLeft
+        ivLeft.scaleType = ImageView.ScaleType.CENTER_CROP
+        ivLeft.setImageResource(R.drawable.lake)
+
+        val ivLeftLayoutParams: ConstraintLayout.LayoutParams = ConstraintLayout.LayoutParams(
+                0, 0)
+        ivLeftLayoutParams.dimensionRatio = "h,16:9"
+        ivLeftLayoutParams.bottomToTop = R.id.guideline
+        ivLeftLayoutParams.startToStart = R.id.cl_root
+        ivLeftLayoutParams.endToEnd = R.id.cl_root
+
+        ivLeft.layoutParams = ivLeftLayoutParams
+
+        constraintLayout.addView(ivLeft)
+
+    }
+
     private fun addViewPartView() {
         val ivLeft = ImageView(this)
         ivLeft.id = R.id.ivLeft
@@ -153,7 +266,7 @@ class DynamicAddViewActivity : AppCompatActivity() {
 
         tvBottomLayoutParams.bottomMargin = ScreenUtil.dpToPx(this, 8)
 
-        tvBottom.layoutParams = tvBottomLayoutParams
+        //tvBottom.layoutParams = tvBottomLayoutParams
 
 
         cl_root.addView(ivLeft)
@@ -171,7 +284,7 @@ class DynamicAddViewActivity : AppCompatActivity() {
         //注意这里和上面的不同
         ivLeftLayoutParams.dimensionRatio = "h,16:9"
         ivLeftLayoutParams.topToTop = R.id.cl_root
-        ivLeft.layoutParams = ivLeftLayoutParams
+        //ivLeft.layoutParams = ivLeftLayoutParams
 
         val tvRightLayoutParams: ConstraintLayout.LayoutParams = ConstraintLayout.LayoutParams(
                 ConstraintLayout.LayoutParams.WRAP_CONTENT,
@@ -182,7 +295,7 @@ class DynamicAddViewActivity : AppCompatActivity() {
         tvRightLayoutParams.marginStart = ScreenUtil.dpToPx(this, 8)
         tvRightLayoutParams.topMargin = ScreenUtil.dpToPx(this, 16)
 
-        tvRight.layoutParams = tvRightLayoutParams
+        //tvRight.layoutParams = tvRightLayoutParams
 
 
         val tvBottomLayoutParams: ConstraintLayout.LayoutParams = ConstraintLayout.LayoutParams(
@@ -200,7 +313,7 @@ class DynamicAddViewActivity : AppCompatActivity() {
 
         tvBottomLayoutParams.bottomMargin = ScreenUtil.dpToPx(this, 8)
 
-        tvBottom.layoutParams = tvBottomLayoutParams
+        //tvBottom.layoutParams = tvBottomLayoutParams
     }
 
     private fun changeConstraintSet() {
